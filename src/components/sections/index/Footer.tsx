70ebf05501fac1f04be065c46a82b696a0b6c756
1dexx1 <121608870+1dexx1@users.noreply.github.com>
2025-02-03 12:54:22 +0200

    Update Footer.tsx

--- src/components/sections/index/Footer.tsx
+++ src/components/sections/index/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <>
             <footer className="max-w-4xl w-full flex flex-col mx-auto pt-4 pb-2">
                 <p className="text-center font-semibold text-base pt-2">
-                    dexx bio - Made with NextJS, TailwindCSS, and ❤ by aiden
+                    dexx bio - Сделано "Конторой пидорасов"
                 </p>
                 <p className="text-center font-medium brightness-75 text-base">
                     Last updated: Feb 3, 2025
