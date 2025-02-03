2cfc239f9fc494ab04733ef8720c4861c3e19635
1dexx1 <121608870+1dexx1@users.noreply.github.com>
2025-02-03 18:04:21 +0200

    Update Projects.tsx

--- src/components/sections/index/Projects.tsx
+++ src/components/sections/index/Projects.tsx
@@ -8,8 +8,22 @@ export default function Projects() {
   const [showAll, setShowAll] = useState(true);
 
   return (
-    <>
-      
+    <>                   
+       <ProjectCard
+            title="taa menu"
+            fullDescription={[
+              "taa меню - это админ меню с закрытыми исходниками, написанное на Glua."
+            cardImage="https://r2.e-z.host/e7a4ea07-ae15-4aaa-9512-6d3ff6fcbeb4/c6pli2jhbcue2pr.png"
+            cardDescription="taa меню - это админ меню с закрытыми исходниками, написанное на Glua."
+            media={[
+                "https://r2.e-z.host/e7a4ea07-ae15-4aaa-9512-6d3ff6fcbeb4/bfabdxuqx8t9xj6.png",
+                "https://r2.e-z.host/e7a4ea07-ae15-4aaa-9512-6d3ff6fcbeb4/hzk2c8mytrohb60.png"
+            ]}
+            myRole="глав. разраб"
+            timeline="янв. 2025 - настоящее время"
+            delay={0.1}
+            gradient="bg-gradient-to-br"
+          />
     </>
   );
 }
