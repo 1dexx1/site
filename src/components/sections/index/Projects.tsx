288a8ead51c2fcd1e98fadfe8bde2d1f394e3577
1dexx1 <121608870+1dexx1@users.noreply.github.com>
2025-02-03 18:07:58 +0200

    Update Projects.tsx

--- src/components/sections/index/Projects.tsx
+++ src/components/sections/index/Projects.tsx
@@ -12,7 +12,8 @@ export default function Projects() {
        <ProjectCard
             title="taa menu"
             fullDescription={[
-              "taa меню - это админ меню с закрытыми исходниками, написанное на Glua.",
+              "taa меню - это админ меню с закрытыми исходниками, написанное на Glua."
+            ]}
             cardImage="https://r2.e-z.host/e7a4ea07-ae15-4aaa-9512-6d3ff6fcbeb4/c6pli2jhbcue2pr.png"
             cardDescription="taa меню - это админ меню с закрытыми исходниками, написанное на Glua."
             media={[
