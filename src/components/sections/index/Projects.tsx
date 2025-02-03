dfcea6b5f51cad15d5e59aa01228d691ff688f4c
1dexx1 <121608870+1dexx1@users.noreply.github.com>
2025-02-03 18:10:05 +0200

    Update Projects.tsx

--- src/components/sections/index/Projects.tsx
+++ src/components/sections/index/Projects.tsx
@@ -8,7 +8,23 @@ export default function Projects() {
   const [showAll, setShowAll] = useState(true);
 
   return (
-    <>                   
+    <>   
+        <section id='projects' className="max-w-4xl w-full flex flex-col mx-auto">
+        <motion.h1
+          className="text-center font-bold text-5xl mt-16 -mb-2"
+          initial={{ transform: 'translateY(-30px)', opacity: 0 }}
+          whileInView={{ transform: 'translateY(0px)', opacity: 100 }}
+          transition={{ duration: 0.5, delay: 0.1, ease: [0.39, 0.21, 0.12, 0.96], }}
+          viewport={{ amount: 0.1, once: true }}
+        >
+          другое
+        </motion.h1>
+        <ul className={`${showAll ? '' : 'max-h-[100rem]'} grid md:grid-cols-2 pt-6 pb-1 grid-cols-1 gap-4 overflow-hidden`}>
+          {!showAll &&
+            <div className="absolute flex justify-center bottom-[5rem] z-10 bg-gradient-to-t from-background pb-8 pt-32 max-w-4xl w-full">
+              <Button label="Show More" onClick={() => setShowAll(true)} width="w-[10rem]" />
+            </div>
+          }
        <ProjectCard
             title="taa menu"
             fullDescription={[
